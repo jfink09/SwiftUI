e4f7bcfa5b4c0186f0b2678e5e2083cf9c997d81
jfink09 <62917477+jfink09@users.noreply.github.com>
2022-05-25 20:19:33 -0400

    Add files via upload
    Obstacle file.

---
+++ Obstacle.swift
@@ -0,0 +1,22 @@
+//
+//  Obstacle.swift
+//  Helicopter
+//
+//  Created by Jason Fink on 5/25/22.
+//  Copyright © 2022 Jason. All rights reserved.
+//
+
+import SwiftUI
+
+struct Obstacle: View {
+    
+    let width: CGFloat = 20
+    let height: CGFloat = 200
+    
+    var body: some View {
+        
+        Rectangle()
+            .frame(width: width, height: height)
+            .foregroundColor(Color.green)
+    }
+}
