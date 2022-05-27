ba1224b8cf9c9c5581bbc8355a5a98c3a34ce528
jfink09 <62917477+jfink09@users.noreply.github.com>
2022-05-27 19:43:53 -0400

    Add files via upload
    Updated WBC.

--- WBC.swift
+++ WBC.swift
@@ -10,13 +10,11 @@ import SwiftUI
 
 struct WBC: View {
     
-    let size: CGFloat
-    let color: Color
+    let size: CGFloat = 40
      
      var body: some View {
          Circle()
              .frame(width: size, height: size)
-             .foregroundColor(color)
+             .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
      }
-
 }
