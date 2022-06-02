c74f6becb548ae27d878117269f33afafc867db3
jfink09 <62917477+jfink09@users.noreply.github.com>
2022-06-01 20:00:17 -0400

    Add files via upload
    Updated position of the WBC and RBC.

--- ContentView.swift
+++ ContentView.swift
@@ -9,21 +9,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+    
+    @State private var wbcPosition = CGPoint(x: 200, y: 700)
+    @State private var obstPosition = CGPoint(x: 200, y: 100)
 
+    var body: some View {
         
         GeometryReader { geo in
             ZStack {
                         Pixel(size: 50, color: Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                            .position(self.wbcPosition)
                         Obstacle()
+                            .position(self.obstPosition)
             }
             .edgesIgnoringSafeArea(.all)
             .frame(width: geo.size.width, height: geo.size.height)
             .background(Color(#colorLiteral(red: 0.3525061763, green: 0, blue: 0, alpha: 0.9)))
         }
-        
-
-        
     }
-
 }
